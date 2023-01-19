318c045423a5ab2fee16900c5a58815e7a42673b
Balu-ch <57792830+Balu-ch@users.noreply.github.com>
2023-01-19 22:04:08 +0530

    Added (home, about, contact, compose) routes for the Blog

--- app.js
+++ app.js
@@ -10,24 +10,28 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+  res.render("home", { home: homeStartingContent });
+});
+app.get("/about", function (req, res) {
+  res.render("about", { about: aboutContent });
+});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contact: contactContent });
+});
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+app.post("/compose", function (req, res) {
+  console.log(req.body.postTitle);
+  console.log(req.body.postBody);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
